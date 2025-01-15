dd6717abfca293b2726539db85e910838ca453c7
Tomdel2106 <113550568+Tomdel2106@users.noreply.github.com>
2025-01-15 20:17:20 +0100

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const themeToggle = document.getElementById('theme-toggle');
-const body = document.body;
-
-themeToggle.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
-
-    // Changer l'icône du bouton en fonction du mode
-    themeToggle.textContent = body.classList.contains('dark-mode') ? '☀️' : '';
-});
